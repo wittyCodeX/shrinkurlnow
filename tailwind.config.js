f4fc0b58a5baafc878a8960ec333b2c2991c1917
Nikolas Manuel <nikolas.manuel13@gmail.com>
2024-10-23 10:51:04 -0500

    Added shadcn, and created the form component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,74 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    darkMode: "class",
-    extend: {
-      colors: {
-        light: {
-          primary: "#B17457",
-          secondary: "#D8D2C2",
-          accent: "#4A4947",
-          neutral: "#FAF7F0",
-        },
-        dark: {
-          primary: "#6A9AB0",
-          secondary: "#3A6D8C",
-          accent: "#EAD8B1",
-          neutral: "#001F3F",
-        },
-      },
-    },
+  	darkMode: 'class',
+  	extend: {
+  		colors: {
+  			light: {
+  				primary: '#B17457',
+  				secondary: '#D8D2C2',
+  				accent: '#4A4947',
+  				neutral: '#FAF7F0'
+  			},
+  			dark: {
+  				primary: '#6A9AB0',
+  				secondary: '#3A6D8C',
+  				accent: '#EAD8B1',
+  				neutral: '#001F3F'
+  			},
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
