8f56dd6150f0be72b011caab770418cce55fb036
Mupa1 <mupasmail@gmail.com>
2024-11-04 06:05:42 +0100

    Add labels and error span element

--- components/forms/Signin.jsx
+++ components/forms/Signin.jsx
@@ -2,41 +2,62 @@
 
 import React from "react";
 import { useForm, FormProvider } from "react-hook-form";
-import { Input } from "../ui/input";
-import { Button } from "../ui/button";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
-function Signin() {
+const Signup = () => {
   const methods = useForm();
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = methods;
 
   const onSubmit = (data) => {
     console.log(data);
   };
 
   return (
-    <div className="flex flex-col max-w-md w-full space-y-4">
-      <h1 className="text-2xl font-bold text-center">Sign In</h1>
+    <section className="flex flex-col max-w-md w-full space-y-4 section">
+      <h1 className="text-2xl font-bold text-center">Sign Up</h1>
       <FormProvider {...methods}>
         <form
-          onSubmit={methods.handleSubmit(onSubmit)}
+          onSubmit={handleSubmit(onSubmit)}
           className="space-y-4 w-full m-auto"
         >
           <Input
             type="email"
-            placeholder="Email"
-            {...methods.register("email")}
+            label="Email"
+            className="focus:outline-none focus:border-light-primary focus:ring-2 focus:ring-light-primary dark:focus:ring-dark-primary dark:focus:border-dark-primary "
+            {...register("email", {
+              required: "Email is required",
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: "Invalid email address",
+              },
+            })}
+            error={errors.email}
           />
           <Input
             type="password"
-            placeholder="Password"
-            {...methods.register("password")}
+            label="Password"
+            className="focus:outline-none focus:border-light-primary focus:ring-2 focus:ring-light-primary dark:focus:ring-dark-primary dark:focus:border-dark-primary "
+            {...register("password", {
+              required: "Password is required",
+              minLength: {
+                value: 8,
+                message: "Password must be at least 8 characters",
+              },
+            })}
+            error={errors.password}
           />
           <Button type="submit" className="w-full">
-            Sign In
+            Sign Up
           </Button>
         </form>
       </FormProvider>
-    </div>
+    </section>
   );
 };
 
-export default Signin;
+export default Signup;
