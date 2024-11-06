07b170b413d8dd3542e82cd94a4c21285643176e
Mupa1 <mupasmail@gmail.com>
2024-11-06 11:03:40 +0100

    Reuse components on signup form and refactor to auth route group

---
+++ app/(auth)/signup/page.jsx
@@ -0,0 +1,10 @@
+import Signup from '@/components/forms/Signup'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <Signup />
+    </div>
+  );
+}
